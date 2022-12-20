8052bafecec86efe8cc7d763a1e51d95daa7dc7f
Lukasz Lisnikowski <l.lisnikowski@gmail.com>
2022-12-20 20:12:13 +0100

    Changed creating objects in SymbolsBar on create in loop

--- UserInterface/symbolsBar.cpp
+++ UserInterface/symbolsBar.cpp
@@ -36,48 +36,27 @@ void SymbolsBar::setNewParentItem(QQuickItem *parentItem)
             [this, parentItem](){this->setHeight(parentItem->height());}
     );
 
+
+
     FactoryLd::create<Ld::EmptyDrop>(this, [this](Ld::EmptyDrop *obj){
         connect(this, &QQuickItem::heightChanged, obj,
                 [this, obj](){obj->setHeight(this->height());});
         obj->setSize({width(), height()});
         obj->setDropValidator(new DropDeleter(obj));
     });
-    FactoryLd::create<ImitatorLd>(this, [this](ImitatorLd *obj){
-        obj->setType(Ld::Type::Contact);
-        obj->setSize({64, 64});
-        obj->setY(0);
-        obj->setDragData(new DragNetworkData(obj, obj->getData(), -1, {}));
-    });
-    FactoryLd::create<ImitatorLd>(this, [this](ImitatorLd *obj){
-        obj->setType(Ld::Type::Coil);
-        obj->setSize({64, 64});
-        obj->setY(64);
-        obj->setDragData(new DragNetworkData(obj, obj->getData(), -1, {}));
-    });
-    FactoryLd::create<ImitatorLd>(this, [this](ImitatorLd *obj){
-        obj->setType(Ld::Type::Timer);
-        obj->setSize({64, 64});
-        obj->setY(64*2);
-        obj->setDragData(new DragNetworkData(obj, obj->getData(), -1, {}));
-    });
-    FactoryLd::create<ImitatorLd>(this, [this](ImitatorLd *obj){
-        obj->setType(Ld::Type::Counter);
-        obj->setSize({64, 64});
-        obj->setY(64*3);
-        obj->setDragData(new DragNetworkData(obj, obj->getData(), -1, {}));
-    });
-    FactoryLd::create<ImitatorLd>(this, [this](ImitatorLd *obj){
-        obj->setType(Ld::Type::Weektimer);
-        obj->setSize({64, 64});
-        obj->setY(64*4);
-        obj->setDragData(new DragNetworkData(obj, obj->getData(), -1, {}));
-    });
-    FactoryLd::create<ImitatorLd>(this, [this](ImitatorLd *obj){
-        obj->setType(Ld::Type::Text);
-        obj->setSize({64, 64});
-        obj->setY(64*5);
-        obj->setDragData(new DragNetworkData(obj, obj->getData(), -1, {}));
-    });
+
+    int i{};
+    for(auto type : {Ld::Type::Contact, Ld::Type::Coil, Ld::Type::Timer,
+                      Ld::Type::Counter, Ld::Type::Weektimer, Ld::Type::Text})
+    {
+        FactoryLd::create<ImitatorLd>(this, [this, type, &i](ImitatorLd *obj){
+            obj->setType(type);
+            obj->setSize({64, 64});
+            obj->setY(64 * i++);
+            obj->setDragData(new DragNetworkData(obj, obj->getData(), -1, {}));
+        });
+    }
+
 }
 
 
--- uTest/UserInterface/SymbolsBar-Test.cpp
+++ uTest/UserInterface/SymbolsBar-Test.cpp
@@ -1,11 +1,8 @@
 #include <gtest/gtest.h>
 
-#include "contact.hpp"
-#include "factoryLd.hpp"
+#include "imitatorLd.hpp"
 #include "qobject.h"
 #include "symbolsBar.hpp"
-#include "contact.hpp"
-#include "coil.hpp"
 
 
 using namespace testing;
@@ -17,8 +14,6 @@ TEST(LD_SymbolsBar, SetNewParentItem)
     QQuickItem parentItem;
 
     symbolsBar.setNewParentItem(&parentItem);
-    auto contact = symbolsBar.findChild<Ld::Contact *>();
-    auto coil = symbolsBar.findChild<Ld::Coil *>();
-    EXPECT_TRUE(contact);
-    EXPECT_TRUE(coil);
+//    auto child = symbolsBar.findChildren<ImitatorLd>();
+//    EXPECT_TRUE(child.count() == 7);
 }
